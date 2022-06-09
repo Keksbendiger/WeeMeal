0497de56e88ff89826e3e01aa0c600f45da3c24a
Danny Steinbrecher <danny.steinbrecher@fh-erfurt.de>
2022-06-09 21:31:08 +0200

    changed android-feature-plugin

--- buildSrc/src/main/java/commons/android-feature.gradle.kts
+++ buildSrc/src/main/java/commons/android-feature.gradle.kts
@@ -5,8 +5,7 @@
 //import implementation
 //import org.gradle.kotlin.dsl.dependencies
 //import org.gradle.kotlin.dsl.project
-//import testImplementation
-//testImplementation
+//
 //plugins {
 //    id("com.android.library")
 //    id("kotlin-android")
@@ -14,100 +13,51 @@
 //}
 //
 //android {
-//
-//    ndkVersion = Version.ndkVersion
-//
-//    compileSdkVersion(Version.compileSdk)
+//    // main config
+//    compileSdk = Config.compile_sdk_version
 //
 //    defaultConfig {
-//        minSdkVersion(Version.minSdk)
-//        targetSdkVersion(Version.targetSdk)
-//        versionCode = Version.versionCode
-//        versionName = Version.versionName
+//        minSdk = Config.min_sdk_version
+//        targetSdk = Config.target_sdk_version
+//
+//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        consumerProguardFiles("consumer-rules.pro")
 //    }
 //
+//
 //    buildTypes {
-//        getByName("release") {
-//            isMinifyEnabled = true
-//            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+//        release {
+//            isMinifyEnabled = false
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
 //        }
 //    }
 //
 //    compileOptions {
-//        sourceCompatibility(JavaVersion.VERSION_1_8)
-//        targetCompatibility(JavaVersion.VERSION_1_8)
+//        sourceCompatibility = JavaVersion.VERSION_1_8
+//        targetCompatibility = JavaVersion.VERSION_1_8
 //    }
 //
 //
 //    kotlinOptions {
-//        jvmTarget = "1.8"
+//        jvmTarget = Config.jvm_target
 //    }
 //
-//    lintOptions {
-//        lintConfig = file("$rootDir/lint.xml")
-//    }
-//
+//    //Compose Config
 //    buildFeatures {
-//        viewBinding = true
+//        compose = true
 //    }
-//}
 //
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = Compose.composeVersion
+//    }
 //
-//dependencies {
-//    implementation(project(Modules.repository))
-//    implementation(project(Modules.interactor))
-//    implementation(project(Modules.model))
-//    implementation(project(Modules.common))
-//
-//    implementation(Dependencies.kotlinStandardLib)
-//    implementation(Dependencies.kotlinReflect)
-//
-//    implementation(Dependencies.koin)
-//    implementation(Dependencies.koinViewModel)
-//    implementation(Dependencies.lifecycleExtension)
-//    implementation(Dependencies.lifecycleViewmodel)
-//    implementation(Dependencies.lifecycleViewmodelSavedState)
-//    implementation(Dependencies.constraintLayout)
-//    implementation(Dependencies.legacySupport)
-//    implementation(Dependencies.material)
-//    implementation(Dependencies.lottie)
-//    implementation(Dependencies.glide)
-//    implementation(Dependencies.flowlayout)
-//    implementation(Dependencies.expandableTextView)
-//    implementation(Dependencies.permission)
-//    implementation(Dependencies.flexBoxLayout)
-//
-//
-//    implementation(Dependencies.appcompat)
-//    implementation(Dependencies.coreKtx)
-//    implementation(Dependencies.fragmentKtx)
-//
-//    implementation(Dependencies.coroutinesCore)
-//    implementation(Dependencies.coroutinesAndroid)
-//    implementation(Dependencies.lifecycleLivedata)
-//
-//    testImplementation(project(Modules.mockFactory))
-//    testImplementation(TestDependencies.core)
-//    testImplementation(TestDependencies.junit)
-//    testImplementation(TestDependencies.androidJUnit)
-//    testImplementation(TestDependencies.koinTesting)
-//    testImplementation(TestDependencies.archCoreTesting)
-//    testImplementation(TestDependencies.coroutinesTest)
-//
-//    androidTestImplementation(TestDependencies.coroutinesTest)
-//}
-//
-//// tasks {
-////     named<Test>("test") {
-////         testLogging {
-////             events("passed", "skipped", "failed")
-////             showStackTraces = true
-////             exceptionFormat = TestExceptionFormat.FULL
-////         }
-////     }
-//// }
-//
-//tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
-//    kotlinOptions.freeCompilerArgs += "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
-//    kotlinOptions.freeCompilerArgs += "-Xopt-in=org.koin.core.component.KoinApiExtension"
+//    packagingOptions {
+//        resources.excludes.apply {
+//            add("META-INF/AL2.0")
+//            add("META-INF/LGPL2.1")
+//        }
+//    }
 //}
