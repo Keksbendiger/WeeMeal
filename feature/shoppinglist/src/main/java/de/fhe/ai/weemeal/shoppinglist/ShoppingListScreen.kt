222cb54aeec4b879129636c492dfcebebd122080
havajafari <hava.jafari@fh-erfurt.de>
2022-07-08 14:52:11 +0200

    refectored Shoppinglist

--- feature/shoppinglist/src/main/java/de/fhe/ai/weemeal/shoppinglist/ShoppingListScreen.kt
+++ feature/shoppinglist/src/main/java/de/fhe/ai/weemeal/shoppinglist/ShoppingListScreen.kt
@@ -1,40 +1,24 @@
 package de.fhe.ai.weemeal.shoppinglist
 
 
-import android.widget.RemoteViews
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListItemInfo
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons.Filled
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import de.fhe.ai.weemeal.common.components.CustomChip
-import de.fhe.ai.weemeal.common.components.ListComponent
-import de.fhe.ai.weemeal.common.components.SearchAppBar
 import de.fhe.ai.weemeal.common.theme.WeeMealTheme
 import de.fhe.ai.weemeal.domain.models.Ingredient
-import de.fhe.ai.weemeal.domain.models.Recipe
 import de.fhe.ai.weemeal.domain.models.ShoppingList
-import de.fhe.ai.weemeal.mocks.ShoppingListMock
+import de.fhe.ai.weemeal.mocks.domain.ShoppingListMock
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 // @Preview
@@ -49,9 +33,7 @@ fun ShoppingListScreen(
 //    onTriggerEvent: (ShoppingListEvents) -> Unit,
 //    onClickOpenShoppingList: (Int) -> Unit,
 ) {
-    WeeMealTheme(
-//        displayProgressBar = shoppingListState.isLoading,
-    ) {
+    WeeMealTheme {
         Scaffold(
             //            topBar = {
             //                AppBar(title = "ShoppingList")
@@ -64,21 +46,15 @@ fun ShoppingListScreen(
                     onClick = { },
                     backgroundColor = MaterialTheme.colors.primary,
                     elevation = FloatingActionButtonDefaults.elevation(6.dp),
-                    text =  {  Text(text = "Teilen") },
-                    icon = { Icon(Filled.Share, "")}
+                    text = { Text(text = "Teilen") },
+                    icon = { Icon(Filled.Share, "") }
                 )
             }
 
         ) { innerPadding ->
             Box(modifier = Modifier.padding(innerPadding)) {
                 Column {
-                    var shoppingList: ShoppingList = ShoppingListMock.generateShoppingList()
-                    shoppingList?.let {
-                        ShoppingList(shoppingList)
-                    } ?: kotlin.run {
-//                        TODO: String ressource location correct?
-                        //Text(stringResource(de.fhe.ai.weemeal.recipe.R.string.no_recipes))
-                    }
+                    ShoppingList(ShoppingListMock.generateList())
 
                 }
             }
@@ -91,7 +67,7 @@ private fun ShoppingList(shoppingList: ShoppingList) {
     LazyColumn {
         itemsIndexed(
             items = shoppingList.items
-        ) { index, ingredient ->
+        ) { _, ingredient ->
             ShoppingListItem(ingredient)
         }
     }
@@ -101,12 +77,12 @@ private fun ShoppingList(shoppingList: ShoppingList) {
 @Composable
 fun ShoppingListItem(ingredient: Ingredient) {
     Card(
-        backgroundColor = MaterialTheme.colors.onBackground,
+        backgroundColor = MaterialTheme.colors.onSecondary,
         modifier = Modifier
             .padding(vertical = 4.dp, horizontal = 8.dp)
 
     ) {
-        Column() {
+        Column {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -131,7 +107,7 @@ fun ShoppingListItem(ingredient: Ingredient) {
                 )
 
                 Text(
-                    text = ingredient.quantity.quantity.toString()+" "+ingredient.quantity.unit,
+                    text = ingredient.quantity.quantity.toString() + " " + ingredient.quantity.unit,
                     style = MaterialTheme.typography.h6.copy(
                         fontWeight = FontWeight.ExtraBold
 
