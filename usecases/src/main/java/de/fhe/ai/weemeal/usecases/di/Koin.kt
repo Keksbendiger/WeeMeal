7fdf9436a6a60cec5497b83b4b2a286a3888f77c
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2022-07-24 21:38:29 +0200

    added usecase to preprare 10 recipes for usability text

--- usecases/src/main/java/de/fhe/ai/weemeal/usecases/di/Koin.kt
+++ usecases/src/main/java/de/fhe/ai/weemeal/usecases/di/Koin.kt
@@ -5,6 +5,7 @@ import de.fhe.ai.weemeal.usecases.meal.SaveMeal
 import de.fhe.ai.weemeal.usecases.recipe.GetRecipeById
 import de.fhe.ai.weemeal.usecases.recipe.GetRecipeByMeal
 import de.fhe.ai.weemeal.usecases.recipe.GetRecipes
+import de.fhe.ai.weemeal.usecases.usabilityTest.PrepareHCIUsabilityTest
 import de.fhe.ai.weemeal.usecases.recipe.SaveRecipe
 import de.fhe.ai.weemeal.usecases.recipe.SearchRecipes
 import de.fhe.ai.weemeal.usecases.weekList.GetFutureMeals
@@ -21,4 +22,7 @@ val usecases = module {
     single { GetRecipeById() }
 
     single { GetMealById() }
+
+
+    single { PrepareHCIUsabilityTest() }
 }
