8d34487920b75c33854986df49e3f3de4f801e88
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2022-07-25 18:03:40 +0200

    - hides bottombar beautifully if not in rootscreen via state - but cant navigate out of not RootScreens...

--- app/src/main/java/de/fhe/ai/weemeal/app/ui/screens/core/AppScaffold.kt
+++ app/src/main/java/de/fhe/ai/weemeal/app/ui/screens/core/AppScaffold.kt
@@ -1,5 +1,6 @@
 package de.fhe.ai.weemeal.app.ui.screens.core
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
@@ -11,16 +12,20 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import de.fhe.ai.weemeal.common.navigation.RootScreens
 import de.fhe.ai.weemeal.common.navigation.Screen
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-val LocalScaffoldState = staticCompositionLocalOf<ScaffoldState> { error("no scaffolded state set") }
+val LocalScaffoldState =
+    staticCompositionLocalOf<ScaffoldState> { error("no scaffolded state set") }
 
+@ExperimentalAnimationApi
 @ExperimentalCoroutinesApi
 @ExperimentalMaterialApi
 @ExperimentalComposeUiApi
@@ -31,15 +36,32 @@ fun AppScaffold() {
     val scaffoldState = rememberScaffoldState()
     var currentScreen by remember { mutableStateOf<Screen>(Screen.Undefined) }
 
+    // State of bottomBar, set state to false, if current page route is not in RootScreens
+    val bottomBarState = rememberSaveable { (mutableStateOf(true)) }
+
+
     CompositionLocalProvider(LocalScaffoldState provides scaffoldState) {
         Scaffold(
             scaffoldState = scaffoldState,
             topBar = { AppBar(currentScreen) },
-            bottomBar = { BottomBar(navController) }
+
+            bottomBar = { BottomBar(navController, bottomBarState = bottomBarState) }
+
+//            Not sufficient... Creates ugly recomposes over the whole screen and you shall never
+//            be able to leave a Screen without bottomnav via back again :(
+//            {
+//                if (currentScreen in RootScreens) {
+//                    BottomBar(navController)
+//                }
+//            }
+
         ) { innerPadding ->
             AppNavigationHost(
                 navController,
                 onNavigation = {
+                    // Only RootScreens shall show Bottombar
+                    bottomBarState.value = currentScreen in RootScreens
+
                     currentScreen = it
                 },
                 modifier = Modifier.padding(innerPadding)
@@ -47,3 +69,98 @@ fun AppScaffold() {
         }
     }
 }
+
+
+/*
+BottomBarAnimationTheme {
+    val navController = rememberNavController()
+
+    // Subscribe to navBackStackEntry, required to get current route
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    // Control TopBar and BottomBar
+    when (navBackStackEntry?.destination?.route) {
+        "cars" -> {
+            // Show BottomBar and TopBar
+            bottomBarState.value = true
+            topBarState.value = true
+        }
+        "bikes" -> {
+            // Show BottomBar and TopBar
+            bottomBarState.value = true
+            topBarState.value = true
+        }
+        "settings" -> {
+            // Show BottomBar and TopBar
+            bottomBarState.value = true
+            topBarState.value = true
+        }
+        "car_details" -> {
+            // Hide BottomBar and TopBar
+            bottomBarState.value = false
+            topBarState.value = false
+        }
+    }
+
+    com.google.accompanist.insets.ui.Scaffold(
+        bottomBar = {
+            BottomBar(
+                navController = navController,
+                bottomBarState = bottomBarState
+            )
+        },
+        topBar = {
+            TopBar(
+                navController = navController,
+                topBarState = topBarState
+            )
+        },
+        content = {
+            NavHost(
+                navController = navController,
+                startDestination = NavigationItem.Cars.route,
+            ) {
+                composable(NavigationItem.Cars.route) {
+                    // show BottomBar and TopBar
+                    LaunchedEffect(Unit) {
+                        bottomBarState.value = true
+                        topBarState.value = true
+                    }
+                    CarsScreen(
+                        navController = navController,
+                    )
+                }
+                composable(NavigationItem.Bikes.route) {
+                    // show BottomBar and TopBar
+                    LaunchedEffect(Unit) {
+                        bottomBarState.value = true
+                        topBarState.value = true
+                    }
+                    BikesScreen(
+                        navController = navController
+                    )
+                }
+                composable(NavigationItem.Settings.route) {
+                    // show BottomBar and TopBar
+                    LaunchedEffect(Unit) {
+                        bottomBarState.value = true
+                        topBarState.value = true
+                    }
+                    SettingsScreen(
+                        navController = navController,
+                    )
+                }
+                composable(NavigationItem.CarDetails.route) {
+                    // hide BottomBar and TopBar
+                    LaunchedEffect(Unit) {
+                        bottomBarState.value = false
+                        topBarState.value = false
+                    }
+                    CarDetailsScreen(
+                        navController = navController,
+                    )
+                }
+            }
+        }
+}
+ */
--- app/src/main/java/de/fhe/ai/weemeal/app/ui/screens/core/BottomBar.kt
+++ app/src/main/java/de/fhe/ai/weemeal/app/ui/screens/core/BottomBar.kt
@@ -1,44 +1,62 @@
 package de.fhe.ai.weemeal.app.ui.screens.core
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import de.fhe.ai.weemeal.common.navigation.RootScreens
 
+@ExperimentalAnimationApi
 @Composable
-fun BottomBar(navController: NavController) {
-    BottomNavigation {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
+fun BottomBar(navController: NavController, bottomBarState: MutableState<Boolean>) {
+    AnimatedVisibility(
+        visible = bottomBarState.value,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = slideOutVertically(targetOffsetY = { it }),
+        content = {
+            BottomNavigation {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
 
-        for (screen in RootScreens) {
-            BottomNavigationItem(
-                icon = { Icon(imageVector = screen.icon, contentDescription = screen.title) },
-                label = {
-                    if (screen.label.isNotEmpty()) {
-                        Text(screen.label)
-                    } else {
-                        Text(screen.title)
-                    }
-                },
-                selected = currentRoute == screen.route,
-                onClick = {
-                    navController.navigate(screen.route) {
-                        navController.graph.startDestinationRoute?.let { route ->
-                            popUpTo(route) {
-                                saveState = true
+                for (screen in RootScreens) {
+                    BottomNavigationItem(
+                        icon = {
+                            Icon(
+                                imageVector = screen.icon,
+                                contentDescription = screen.title
+                            )
+                        },
+                        label = {
+                            if (screen.label.isNotEmpty()) {
+                                Text(screen.label)
+                            } else {
+                                Text(screen.title)
+                            }
+                        },
+                        selected = currentRoute == screen.route,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                navController.graph.startDestinationRoute?.let { route ->
+                                    popUpTo(route) {
+                                        saveState = true
+                                    }
+                                }
+                                launchSingleTop = true
+                                restoreState = true
                             }
                         }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
+                    )
                 }
-            )
+            }
         }
-    }
+    )
 }
