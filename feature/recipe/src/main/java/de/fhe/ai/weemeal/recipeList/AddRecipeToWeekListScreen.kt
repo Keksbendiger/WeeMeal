cf0c625ef4b4a0454bd88c77015a053cc562e7f4
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2022-07-28 09:36:59 +0200

    ktlint

--- feature/recipe/src/main/java/de/fhe/ai/weemeal/recipeList/AddRecipeToWeekListScreen.kt
+++ feature/recipe/src/main/java/de/fhe/ai/weemeal/recipeList/AddRecipeToWeekListScreen.kt
@@ -3,7 +3,6 @@ package de.fhe.ai.weemeal.recipeList
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
