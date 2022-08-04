bcc2645778635c43c8758656551ed57aa8645baa
Keksbendiger <keksbendiger@gmail.com>
2022-08-04 10:44:56 +0200

    ktlint

--- feature/meal/src/main/java/de/fhe/ai/weemeal/mealDetail/MealDetailsScreen.kt
+++ feature/meal/src/main/java/de/fhe/ai/weemeal/mealDetail/MealDetailsScreen.kt
@@ -27,9 +27,7 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
